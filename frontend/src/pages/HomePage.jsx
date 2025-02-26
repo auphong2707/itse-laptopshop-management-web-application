3b71ca96e323ffe73c3871fa635f6eadb59eab40
veil <auphong2707@gmail.com>
2025-02-26 15:14:54 +0700

    Add Swipe advertisement

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
-import { Layout } from "antd";
+import { Layout, Image } from "antd";
 import WebsiteHeader from "./components/WebsiteHeader";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const { Content } = Layout;
+
+const images = [
+  "None",
+  "None",
+  "None",
+]
+
+const contentStyle = {
+  color: '#fff',
+  backgroundColor: 'white',
+};
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 5000 }}
+        style={{ height: "300px", backgroundColor: "white" }}
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={img}
+              height="100%"
+              width="100%"
+              preview={false}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
 
 const HomePage = () => {
   return (
     <Layout>
+      {/* Header */}
       <WebsiteHeader />
+
+      {/* Main Content */}
+      <Content className='responsive-padding' style={contentStyle}>
+        <ImageGallery />
+      </Content>
+
+      {/* Footer */}
+
     </Layout>
   );
 }
