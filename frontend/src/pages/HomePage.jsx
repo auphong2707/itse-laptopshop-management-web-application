20aeb8366818925433890be17ae8182271184844
veil <auphong2707@gmail.com>
2025-02-26 14:56:36 +0700

    Create WebsiteHeader

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from "antd";
+import WebsiteHeader from "./components/WebsiteHeader";
+
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <WebsiteHeader />
+    </Layout>
+  );
+}
+
+export default HomePage;
