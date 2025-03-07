250cc7d1f9806d10dbbc5e5d65239dd9835bed74
veil <auphong2707@gmail.com>
2025-03-07 18:54:37 +0700

    Fix the database initial function

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 cd ./frontend && npm install && cd ../backend && pip install -r requirements.txt
 
 # Create tables, insert sample data
+psql -h localhost -U postgres -d postgres -f ./commands/clear_database.sql
 psql -h localhost -U postgres -d postgres -f ./commands/create_table.sql
-psql -h localhost -U postgres -d postgres -f ./commands/data_sample.sql
+psql -h localhost -U postgres -d postgres -f ./commands/insert_sample_data.sql
