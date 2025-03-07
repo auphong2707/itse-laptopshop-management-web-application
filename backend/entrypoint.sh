f68e57a0f4484364fa1d27e303939f72ca36573d
veil <auphong2707@gmail.com>
2025-03-07 18:10:36 +0700

    Fix entrypoint.sh

--- backend/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-psql -h localhost -U postgres -d postgres -f backend/commands/create_table.sql
-# psql -h localhost -U postgres -d postgres -f backend/commands/data_sample.sql
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+# Install dependencies
 cd ./frontend && npm install && cd ../backend && pip install -r requirements.txt
-psql -h localhost -U postgres -d postgres < ./commands.sql
+
+# Create tables, insert sample data
+psql -h localhost -U postgres -d postgres -f ./commands/create_table.sql
+psql -h localhost -U postgres -d postgres -f ./commands/data_sample.sql
