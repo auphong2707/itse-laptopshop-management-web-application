810c8c8947905c37b51476ec9a799e482391a78f
Trần Sỹ Minh Quân <minhquantsy@gmail.com>
2025-03-08 19:51:33 +0700

    change padding

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -44,7 +44,7 @@ const ExtraInfo = () => (
 );
 
 const AboutProduct = () => (
-  <div style={{ maxWidth: "60%", paddingLeft: "5%", paddingRight: "5%" }}>
+  <div style={{ maxWidth: "100%", paddingLeft: "12%", paddingRight: "0%" }}>
     <ProductHeader title="MSI MPG Trident 3" series="MSI MPG Series" />
     <p style={{ marginTop: "1rem", fontSize: "16px", lineHeight: "1.6" }}>
     MSI MPG Trident 3 10SC-005AU Intel i7 10700F, 2060 SUPER, 16GB RAM, 512GB SSD, 2TB HDD, Windows 10 Home, Gaming Keyboard and Mouse 3 Years Warranty Gaming Desktop
@@ -54,7 +54,7 @@ const AboutProduct = () => (
 );
 
 const Details = () => (
-  <div style={{ maxWidth: "60%", paddingLeft: "5%", paddingRight: "5%" }}>
+  <div style={{ maxWidth: "100%", paddingLeft: "12%", paddingRight: "0%" }}>
     <ProductHeader title="MSI MPG Trident 3" series="MSI MPG Series" />
     <ul style={{ marginTop: "1rem", fontSize: "16px", lineHeight: "1.8" }}>
       <li>Intel Core i7-10700F</li>
@@ -70,7 +70,7 @@ const Details = () => (
 );
 
 const Specs = () => (
-  <div style={{ maxWidth: "60%", paddingLeft: "5%", paddingRight: "5%" }}>
+  <div style={{ maxWidth: "100%", paddingLeft: "12%", paddingRight: "0%" }}>
     <ProductHeader title="MSI MPG Trident 3" series="MSI MPG Series" />
     <Table
       columns={[
@@ -93,18 +93,33 @@ const ProductPage = () => {
   return (
     <Layout>
       <WebsiteHeader />
-      <Content className="responsive-padding" style={{ backgroundColor: "#fff", padding: "0rem " }}>
+      <Content className="responsive-padding" style={{ 
+        backgroundColor: "#fff", 
+        padding: "0rem", 
+        display: "flex", 
+        justifyContent: "space-between", 
+        alignItems: "center",
+        height: "100%"
+      }}>
+
         <Purchase />
+        {/* Left Side: Product Tabs */}
+        <div style={{ width: "50%", paddingLeft: "7%" }}>
+          <ProductTabs
+            tabLabels={["About Product", "Details", "Specs"]}
+            tabContents={[
+              AboutProduct(),
+              Details(),
+              Specs()
+            ]}
+          />
+        </div>
+
+        {/* Right Side: Product Image */}
         <ProductImage />
-        <ProductTabs
-          tabLabels={["About Product", "Details", "Specs"]}
-          tabContents={[
-            AboutProduct(),
-            Details(),
-            Specs()
-          ]}
-        />
+
       </Content>
+
 
       {/* Advertisement */}
       <ImageGallery imageSources={imageSources} />
--- frontend/src/pages/components/ProductImage.jsx
+++ frontend/src/pages/components/ProductImage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 import { HeartOutlined, BarChartOutlined, MailOutlined } from "@ant-design/icons";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
@@ -15,41 +14,51 @@ const imageSources = [
 
 const ProductImage = () => {
   return (
-    <div style={{ 
-      position: "absolute", 
-      top: "100px",  
-      right: "-10px", 
-      textAlign: "center", 
-      display: "inline-block", 
-      padding: "20px", 
-      maxWidth: "550px",
-      marginTop: "4rem",
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "20px", // Khoảng cách giữa ảnh và button
+      paddingRight: "8rem",
+      paddingTop: "0rem",
     }}>
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        pagination={{ clickable: true, dynamicBullets: true }}
-        autoplay={{ delay: 5000 }}
-        style={{ height: "400px", backgroundColor: "white", paddingBottom: "20px" }}
-      >
-        {imageSources.map((img, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={img}
-              alt={`Product ${index + 1}`}
-              style={{ width: "100%", height: "auto", objectFit: "contain" }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      {/* Icons Container */}
-      <div style={{ position: "absolute", top: "30%", left: "10px", transform: "translateY(-50%)", display: "flex", flexDirection: "column", gap: "10px", zIndex: "1000" }}> 
+      
+      {/* Icons Container - Chuyển sang bên trái ảnh */}
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        zIndex: "10",
+        position: "relative", // Đặt lại relative thay vì absolute
+        marginRight: "10px", // Dịch sang phải gần ảnh hơn
+      }}>
         <IconButton icon={<HeartOutlined />} tooltip="Add to Wishlist" />
         <IconButton icon={<BarChartOutlined />} tooltip="Compare" />
         <IconButton icon={<MailOutlined />} tooltip="Send Inquiry" />
       </div>
+
+      {/* Swiper Image Gallery */}
+      <div style={{ width: "400px", height: "450px", position: "relative" }}>
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          spaceBetween={10}
+          slidesPerView={1}
+          pagination={{ clickable: true, dynamicBullets: true }}
+          autoplay={{ delay: 5000 }}
+          style={{ height: "100%", width: "100%", backgroundColor: "white" }}
+        >
+          {imageSources.map((img, index) => (
+            <SwiperSlide key={index} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <img
+                src={img}
+                alt={`Product ${index + 1}`}
+                style={{ width: "100%", height: "100%", objectFit: "contain" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      
     </div>
   );
 };
@@ -59,14 +68,14 @@ const IconButton = ({ icon, tooltip }) => {
   return (
     <div
       style={{
-        width: "40px", 
-        height: "40px", 
-        borderRadius: "50%", 
-        backgroundColor: "#f5f5f5", 
-        display: "flex", 
-        alignItems: "center", 
-        justifyContent: "center", 
-        cursor: "pointer", 
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+        backgroundColor: "#f5f5f5",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: "pointer",
         boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
         transition: "background-color 0.3s ease, transform 0.2s ease",
       }}
