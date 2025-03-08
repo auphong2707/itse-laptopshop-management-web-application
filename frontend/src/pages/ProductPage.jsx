6c9c05a444489c4c309ae08a8656887d599762c5
Trần Sỹ Minh Quân <minhquantsy@gmail.com>
2025-03-08 18:26:27 +0700

    Update ProductPage.jsx

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -47,6 +47,7 @@ const AboutProduct = () => (
   <div style={{ maxWidth: "60%", paddingLeft: "10%", paddingRight: "5%" }}>
     <ProductHeader title="MSI MPG Trident 3" series="MSI MPG Series" />
     <p style={{ marginTop: "1rem", fontSize: "16px", lineHeight: "1.6" }}>
+    MSI MPG Trident 3 10SC-005AU Intel i7 10700F, 2060 SUPER, 16GB RAM, 512GB SSD, 2TB HDD, Windows 10 Home, Gaming Keyboard and Mouse 3 Years Warranty Gaming Desktop
     </p>
     <ExtraInfo />
   </div>
