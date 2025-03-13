bb94ea51b561e72eb729e67e132b492050998d14
veil <auphong2707@gmail.com>
2025-03-13 20:33:05 +0700

    Set up ElasticSearch

--- backend/reset_database.sh
+++ backend/reset_database.sh
@@ -3,6 +3,6 @@ set -e
 
 python backend/commands/generate_sample_data.py
 
-psql -h localhost -U postgres -d postgres -f backend/commands/clear_database.sql
-psql -h localhost -U postgres -d postgres -f backend/commands/create_table.sql
-psql -h localhost -U postgres -d postgres -f backend/commands/insert_sample_data.sql
+PGPASSWORD=postgres psql -h db -U postgres -d postgres -f backend/commands/clear_database.sql
+PGPASSWORD=postgres psql -h db -U postgres -d postgres -f backend/commands/create_table.sql
+PGPASSWORD=postgres psql -h db -U postgres -d postgres -f backend/commands/insert_sample_data.sql
