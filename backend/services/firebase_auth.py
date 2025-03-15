c1805441f11487c53272369765b3026c844e119b
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2025-03-15 22:40:18 +0700

    Create firebase_auth.py

---
+++ backend/services/firebase_auth.py
@@ -0,0 +1,11 @@
+class ExtendedUserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    display_name: Optional[str] = None
+    phone_number: str
+    first_name: str
+    last_name: str
+    company: Optional[str] = None
+    address: Optional[str] = None
+    country: str
+    zip_code: str
