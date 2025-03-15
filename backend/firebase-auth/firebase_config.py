6fa83dee9e93c77ee42eaebb657c4e157f50cf28
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2025-03-15 22:40:13 +0700

    delele old firebase folder

--- backend/firebase-auth/firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth, firestore
-
-# Khởi tạo Firebase Admin SDK
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-
-# Kết nối Firestore
-db = firestore.client()
