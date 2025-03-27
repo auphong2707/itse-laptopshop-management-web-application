9e8d25c2f253393963715d8ae3a09b095c0ff2be
veil <auphong2707@gmail.com>
2025-03-27 20:27:29 +0700

    Add remove schedule for delete_logs table

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,5 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # Install dependencies
 cd ./frontend && npm install && cd ../backend && pip install -r requirements.txt && cd ..
 
-# Install pg-cron
-PGPASSWORD=postgres psql -h db -U postgres -d postgres -c "CREATE EXTENSION pg_cron;" || {
-    echo "Warning: Failed to install pg_cron. Continuing..."
-}
-
 # Create tables, insert sample data (always runs)
 ./backend/reset_database.sh
