6bd958d943db0da61910a1fd4b951f07d504bc3a
veil <auphong2707@gmail.com>
2025-03-27 20:13:57 +0700

    Install pg_cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
 
 # Install dependencies
 cd ./frontend && npm install && cd ../backend && pip install -r requirements.txt && cd ..
 
-# Create tables, insert sample data
+# Install pg-cron
+PGPASSWORD=postgres psql -h db -U postgres -d postgres -c "CREATE EXTENSION pg_cron;" || {
+    echo "Warning: Failed to install pg_cron. Continuing..."
+}
+
+# Create tables, insert sample data (always runs)
 ./backend/reset_database.sh
