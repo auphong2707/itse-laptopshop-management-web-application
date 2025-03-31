7546dc162fbc19f98a5442d92f00a91983d24368
Trần Sỹ Minh Quân <minhquantsy@gmail.com>
2025-03-31 11:49:24 +0700

    Update ImageGallery.jsx

--- frontend/src/pages/components/ImageGallery.jsx
+++ frontend/src/pages/components/ImageGallery.jsx
@@ -1,82 +1,41 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import { Image } from "antd";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import ColorThief from "color-thief-browser";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import PropTypes from "prop-types";
 
-const ImageGallery = ({ imageSources }) => {
-  const [bgColor, setBgColor] = useState("#ffffff");
-  const imageRef = useRef(null);
 
-  useEffect(() => {
-    if (imageSources.length > 0) {
-      extractColor(imageSources[0]); // Extract color for the first image
-    }
-  }, [imageSources]);
-
-  const extractColor = async (imgSrc) => {
-    try {
-      const img = new Image();
-      img.crossOrigin = "Anonymous";
-      img.src = imgSrc;
-
-      img.onload = () => {
-        try {
-          const colorThief = new ColorThief();
-          if (img.complete) {
-            const dominantColor = colorThief.getColor(img);
-            setBgColor(`rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`);
-          }
-        } catch (error) {
-          console.error("ColorThief error:", error);
-        }
-      };
-
-      img.onerror = () => console.error("Failed to load image:", imgSrc);
-    } catch (error) {
-      console.error("extractColor error:", error);
-    }
-  };
-
+const ImageGallery = ({imageSources}) => {
   return (
-    <div
-      style={{
-        maxWidth: "100%",
-        margin: "auto",
-        backgroundColor: bgColor,
-        transition: "background-color 0.5s ease-in-out",
-      }}
-    >
+    <div>
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={10}
+        spaceBetween={50}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         autoplay={{ delay: 5000 }}
-        onSlideChange={(swiper) => extractColor(imageSources[swiper.activeIndex])}
-        style={{ height: "auto" }}
+        style={{ height: "300px", backgroundColor: "white" }}
       >
         {imageSources.map((img, index) => (
-          <SwiperSlide key={index} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <img
-              ref={index === 0 ? imageRef : null}
+          <SwiperSlide key={index}>
+            <Image
               src={img}
-              height={300}
+              height="100%"
               width="100%"
               style={{ objectFit: "contain" }}
-              alt={`Slide ${index + 1}`}
+              preview={false}
             />
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
-  );
-};
+  )
+}
 
 ImageGallery.propTypes = {
   imageSources: PropTypes.arrayOf(PropTypes.string),
